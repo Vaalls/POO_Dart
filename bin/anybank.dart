0acf50a5a4e2877a168e0e1f2f7a822894bfe29b
Vaalls <bielvalls4@gmail.com>
2024-10-09 19:22:14 -0300

    Criando funções de transferência de valores.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,25 +1,32 @@
 void main(){
-  Conta contaGabriel = Conta("Gabriel", 20000);
-  Conta contaBeatriz = Conta("Beatriz", 15000);
+  Conta contaGabriel = Conta("Gabriel", 200);
+  Conta contaBeatriz = Conta("Beatriz", 150);
 
   List<Conta> contas = <Conta>[contaGabriel, contaBeatriz];
-
-  //Acessando valores
-  print(contaGabriel.titular);
-  print(contaGabriel.saldo);
-
-  //Atualizando valores
-  print("Antes da alteração: ${contaBeatriz.saldo} ");
-  contaBeatriz.saldo = 500;
-  print("Após atualizar o saldo: ${contaBeatriz.saldo}");
-
-  print("");
 
   for (var conta in contas) {
     print(conta.titular);
     print(conta.saldo);
   }
 
+  receber(contaGabriel, 100);
+  print(contaGabriel.titular);
+  print(contaGabriel.saldo);
+
+  enviar(contaGabriel, 50);
+
+  print("Apos enviar");
+  print(contaGabriel.titular);
+  print(contaGabriel.saldo);
+
+}
+
+void enviar(Conta conta, double valor){
+  conta.saldo -= valor;
+}
+
+void receber(Conta conta, double valor){
+  conta.saldo += valor;
 }
 
 class Conta{
