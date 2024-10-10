8cb429ea37763b2f30cbd7a3b38f3b2c267c2466
Vaalls <bielvalls4@gmail.com>
2024-10-10 11:51:02 -0300

    Utilizando herença para os novos tipos de conta que temos em nosso banco. Instanciamos objetos e testamos.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main(){
   Conta contaGabriel = Conta("Gabriel", 200);
   Conta contaBeatriz = Conta("Beatriz", 150);
+  ContaCorrente contaGustavo = ContaCorrente("Gustavo", 400);
+  ContaPoupanca contaKaio = ContaPoupanca("Kaio", 700);
 
   List<Conta> contas = <Conta>[contaGabriel, contaBeatriz];
 
@@ -13,6 +15,12 @@ void main(){
   contaGabriel.receber(100);
   contaBeatriz.receber(200);
   contaGabriel.enviar(10);
+
+  contaGustavo.receber(100);
+  contaGustavo.imprimeSaldo();
+
+  contaKaio.enviar(200);
+  contaKaio.imprimeSaldo();
 
 }
 
