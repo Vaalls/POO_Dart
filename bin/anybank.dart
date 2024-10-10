ca0ba1cfdf9cdb8cccdb7a56f14bd102c82c5299
Vaalls <bielvalls4@gmail.com>
2024-10-10 12:11:25 -0300

    Aplicando o conceito de abstração em nossa classe "molde".

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,20 +1,8 @@
 import 'package:anybank/conta.dart';
 
 void main(){
-  Conta contaGabriel = Conta("Gabriel", 200);
-  Conta contaBeatriz = Conta("Beatriz", 150);
   ContaCorrente contaGustavo = ContaCorrente("Gustavo", 4000);
   ContaPoupanca contaKaio = ContaPoupanca("Kaio", 4000);
-
-  List<Conta> contas = <Conta>[contaGabriel, contaBeatriz];
-
-  for (var conta in contas) {
-    conta.imprimeSaldo();
-  }
-
-  contaGabriel.receber(100);
-  contaBeatriz.receber(200);
-  contaGabriel.enviar(10);
 
   contaGustavo.imprimeSaldo();
   contaGustavo.enviar(4300);
@@ -24,6 +12,5 @@ void main(){
   contaKaio.calculaRendimento();
   contaKaio.imprimeSaldo();
   
-
 }
 
