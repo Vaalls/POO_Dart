44e47902806be18d0a4f3f64b51b21df8f3f29a5
Vaalls <bielvalls4@gmail.com>
2024-10-10 12:31:19 -0300

    Aprofundando em mixins.

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main(){
   ContaCorrente contaGustavo = ContaCorrente("Gustavo", 4000);
   ContaPoupanca contaKaio = ContaPoupanca("Kaio", 4000);
+  ContaEmpresa contaGabriel = ContaEmpresa("Gabriel", 5000);
+  ContaInvestimento contaBea = ContaInvestimento("Beatriz", 5000);
 
   contaGustavo.imprimeSaldo();
   contaGustavo.enviar(4300);
@@ -11,6 +13,9 @@ void main(){
   contaKaio.enviar(4300);
   contaKaio.calculaRendimento();
   contaKaio.imprimeSaldo();
+
+  contaGabriel.enviar(1000);
+  contaBea.receber(1000);
   
 }
 
